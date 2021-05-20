ffb80b3e5385edd13c53ddba2d991ebc9c5a98f0
Paul Roberts <paul.roberts@theguardian.com>
2021-05-20 11:57:49 +0100

    capture failure and log

--- src/main/scala/ophan/geoip/db/refresher/Lambda.scala
+++ src/main/scala/ophan/geoip/db/refresher/Lambda.scala
@@ -4,6 +4,8 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
 import ophan.geoip.db.refresher.logging.Logging
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import scala.util.Failure
+import scala.util.Success
 
 object Lambda extends Logging {
 
@@ -15,22 +17,24 @@ object Lambda extends Logging {
   }
 
   private def retrieveAndStore(maxmindDatabaseEdition: MaxmindDatabaseEdition) = {
-    for {
+    (for {
       downloadedArchiveFile <- MaxmindArchiveDownloader.download(maxmindDatabaseEdition)
-    } {
-      maxmindDatabaseEdition.usingDatabaseStreamFrom(downloadedArchiveFile) { streamOfKnownSize =>
+      _ <- maxmindDatabaseEdition.usingDatabaseStreamFrom(downloadedArchiveFile) { streamOfKnownSize =>
         val s3Key = s"geoip/test-${maxmindDatabaseEdition.databaseFileName}"
         val objectRequest = PutObjectRequest.builder.bucket("ophan-dist").key(s3Key).build
 
         logger.info(Map(
-            "databaseFile.size" -> streamOfKnownSize.size,
-            "databaseFile.s3.destination" -> s3Key
-          ), s"Starting upload of ${streamOfKnownSize.size} bytes to S3 ($objectRequest)")
+          "databaseFile.size" -> streamOfKnownSize.size,
+          "databaseFile.s3.destination" -> s3Key
+        ), s"Starting upload of ${streamOfKnownSize.size} bytes to S3 ($objectRequest)")
         val putObjectResponse = AWS.S3.putObject(objectRequest, streamOfKnownSize.asAwsSdkRequestBody())
         logger.info(Map(
-            "databaseFile.s3.versionId" -> putObjectResponse.versionId
-          ), s"Upload complete!")
+          "databaseFile.s3.versionId" -> putObjectResponse.versionId
+        ), s"Upload complete!")
       }
+    } yield ()) match {
+      case Failure(err) => logger.error(s"Failure to retrieve url", err)
+      case _:Success[_] =>
     }
   }
 
@@ -42,4 +46,3 @@ object Lambda extends Logging {
   }
 
 }
-
