3cd569dd91d2daa8dd9f37c4d9e81e5e2cf7906e
Dina Hafez <dina.hafez@guardian.co.uk>
2023-02-21 09:22:25 +0000

    Added CI to GuCDK geoip db refresher

---
+++ scripts/ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+DIR=$(cd `dirname $0` && pwd)
+
+pushd $DIR
+cd ..
+
+(
+  cd cdk
+  npm ci
+  npm run lint
+  npm test
+  npm run synth
+)
+
+sbt clean compile test dist
+
+popd
