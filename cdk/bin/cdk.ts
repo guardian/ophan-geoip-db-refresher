9629c670d9831b72b658f330c8aacd82dcc0098f
Dina Hafez <dina.hafez@guardian.co.uk>
2023-02-28 14:18:49 +0000

    Sets CDK repo tag
    Co-Authored-By: Sam Hession <4633246+SHession@users.noreply.github.com>

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,14 @@
-import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { GeoipDbRefresher } from "../lib/geoip-db-refresher";
+import 'source-map-support/register';
+import { ContextKeys } from '@guardian/cdk/lib/constants';
+import { App } from 'aws-cdk-lib';
+import { GeoipDbRefresher } from '../lib/geoip-db-refresher';
 
-const app = new App();
-new GeoipDbRefresher(app, "GeoipDbRefresher-PROD", { stack: "ophan", stage: "PROD" });
+const app = new App({
+	context: {
+		[ContextKeys.REPOSITORY_URL]: 'guardian/ophan-geoip-db-refresher',
+	},
+});
+new GeoipDbRefresher(app, 'GeoipDbRefresher-PROD', {
+	stack: 'ophan',
+	stage: 'PROD',
+});
